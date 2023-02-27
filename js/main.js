ae82dd9e41fbe3ab0e02c403a0f81b96b99a06de
VasylivR <vasylivromana@gmail.com>
2023-02-28 00:44:12 +0200

    get all id elements

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const timeEl = document.querySelector('#time');
+const dateEl = document.querySelector('#date');
+const currentWeatheritemsEl = document.querySelector('#current-weather-items');
+const timeZone = document.querySelector('#time-zone');
+const countryEl = document.querySelector('#country');
+const weatherForecastEl = document.querySelector('#wether-forecast');
+const weatherTempEl = document.querySelector('#current-temp');
